647061eb256cdc48c2f9baaa1d368a0f619692a2
Reemaalhawas <93104215+Reemaalhawas@users.noreply.github.com>
2025-02-06 02:33:51 +0300

    Implement user registration with hashed passwords

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from datetime import datetime, timedelta
 import jwt
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException
+from Databases import get_mysql_connection, password_hasher
+from fastapi import HTTPException
+
 
 app = FastAPI()
 
@@ -23,5 +27,25 @@ def generate_jwt(payload: dict, expires_delta: timedelta = None):
 
 
 token_auth = OAuth2PasswordBearer(tokenUrl="login")
+
+
+@app.post("/signup/")
+def create_account(username: str, email: str, password: str):
+    conn = get_mysql_connection()
+    cursor = conn.cursor()
+
+    hashed_pw = password_hasher.hash(password)
+
+    try:
+        cursor.execute("INSERT INTO users (username, email, hashed_password) VALUES (%s, %s, %s)", 
+                       (username, email, hashed_pw))
+        conn.commit()
+    except:
+        raise HTTPException(status_code=400, detail="Username or email already taken.")
+    finally:
+        cursor.close()
+        conn.close()
+
+    return {"message": "User successfully registered"}
 
 
