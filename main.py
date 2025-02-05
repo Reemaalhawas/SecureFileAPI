7165a7a4d7771d650971768190d48cb408251d09
Reemaalhawas <93104215+Reemaalhawas@users.noreply.github.com>
2025-02-06 01:56:40 +0300

    Add OAuth2 token authentication

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from datetime import datetime, timedelta
 import jwt
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
@@ -18,4 +19,9 @@ def generate_jwt(payload: dict, expires_delta: timedelta = None):
     expiry = datetime.utcnow() + (expires_delta or timedelta(minutes=TOKEN_EXPIRATION_MINUTES))
     payload.update({"exp": expiry})
     return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+
+
+token_auth = OAuth2PasswordBearer(tokenUrl="login")
+
 
